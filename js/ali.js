c54e5b924805871d4e3f473dae23b9cabcb9944a
jianh-ai <70783403+jianh-ai@users.noreply.github.com>
2024-09-10 22:36:23 +0800

    Update ali.js

--- js/ali.js
+++ js/ali.js
@@ -1,14 +1,11 @@
 // 2024-09
-console.log("User-Agent:", ua);
-// console.log($request.headers);
-// 获取 User-Agent
-var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
+var ua = $request.headers['User-Agent'];
 
-// 检查 User-Agent 中是否包含特定关键词
-if (/(AMap|Cainiao|%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C|%E5%96%B5%E8%A1%97|%E5%A4%A9%E7%8C%AB|Hema4iPhone|Moon|DMPortal)/.test(ua)) {
-  // 阻止请求，不返回内容
-  $done({status: "HTTP/1.1 404 Not Found"});
+// 判断是否包含关键字 'Amap'
+if (ua.includes('Amap')) {
+    // 返回404状态码
+    $done({ response: { status: 404, body: 'Not Found' } });
 } else {
-  // 允许其他请求通过
-  $done({});
+    // 继续请求
+    $done({});
 }
