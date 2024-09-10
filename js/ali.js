c5d27f613cbf83a64a06f60c4cf85b174e4c2332
jianh-ai <70783403+jianh-ai@users.noreply.github.com>
2024-09-10 22:42:41 +0800

    Update ali.js

--- js/ali.js
+++ js/ali.js
@@ -1,11 +1,13 @@
 // 2024-09
+// 获取请求的 URL 和 User-Agent
 var ua = $request.headers['User-Agent'];
+var url = $request.url;
 
-// 判断是否包含关键字 'Amap'
-if (ua.includes('Amap')) {
-    // 返回404状态码
-    $done({ response: { status: 404, body: 'Not Found' } });
+// 判断是否是指定的 URL 并且 UA 包含 "Amap"
+if (url.includes("http://amdc.m.taobao.com/amdc/mobileDispatch") && ua.includes("Amap")) {
+    // 返回 404 状态码，拒绝请求
+    $done({ response: { status: 404, body: "Request blocked by script" } });
 } else {
-    // 继续请求
+    // 继续正常处理请求
     $done({});
 }
